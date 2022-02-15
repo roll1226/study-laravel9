688db152c04b22d0f59ccdd482e37000edbe416f
roll1226 <tomo_yasu_4724@icloud.com>
2022-02-15 12:15:31 +0900

    feat: vuetify

--- backend/resources/js/app.ts
+++ backend/resources/js/app.ts
@@ -1,8 +1,14 @@
+import "vuetify/styles";
 import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import ExampleComponent from "./components/App.vue";
 
 require("./bootstrap");
 
 const app = createApp(ExampleComponent);
+const vuetify = createVuetify({ components, directives });
 
+app.use(vuetify);
 app.mount("#app");
--- backend/resources/js/components/app.vue
+++ backend/resources/js/components/app.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>{{ message }}</div>
+    <v-app>
+        <v-card>
+            <v-card-title>{{ message }}</v-card-title>
+        </v-card>
+    </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-
 export default defineComponent({
     setup() {
         const message = ref<string>("Hello World");
